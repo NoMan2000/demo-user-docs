5e5807c8ef8f572abd38652ff64012a0622ecc14
NoMan2000 <webart.video@gmail.com>
2020-10-26 12:07:56 -0700

    Sure
    Signed-off-by: NoMan2000 <webart.video@gmail.com>

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '26255d151579468a039b65324f843328'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '201dcd1c-9f6c-4d70-80f1-6ee933034b3f'
+};
